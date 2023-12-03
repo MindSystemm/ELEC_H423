c5358a063e124c960d8cd974bc1be1dc85f52854
yente06 <yente.cool@gmail.com>
2023-12-03 21:28:11 +0100

    Update Config.h

--- ESP/Config.h
+++ ESP/Config.h
@@ -1,3 +1,6 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
 // Identification
 #define ID_ESP "Sensor 1"
 
@@ -8,3 +11,8 @@
 // MQTT information
 #define MQTT_ADDRESS_ESP "x.x.x.x"
 #define MQTT_PORT_ESP 1883
+
+// Encryption key (128 bits)
+#define CRYPTO_KEY {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}
+
+#endif
